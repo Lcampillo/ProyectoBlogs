c04aecfa1d44086b3db012a78c390a9ea97a7b15
Lcampillo <75735317+Lcampillo@users.noreply.github.com>
2020-12-20 10:51:58 -0500

    Envio de correo de activación

--- controllers/AuthController.py
+++ controllers/AuthController.py
@@ -1,6 +1,11 @@
 from app import *
 from models import Auth
 from flask import render_template
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+
+msg = MIMEMultipart()
 
 @app.route('/login')
 def login():
@@ -27,6 +32,24 @@ def store():
 
       result = hazEl.register()
 
+      message_e = f'''Bienvenido a Bloggi!!\nHola {name}, nos place que empieces a utilizar nuestros servicios de Blog\npara ellos solcitamos
+      que actives tu cuenta de usario en el siguiente enlace:\n url=https'''
+      #parametros de conexión del correo electronico
+      password = "yoyito2020"
+      msg['From'] = "profeyoyito2020@gmail.com"
+      msg['To'] = email
+      msg['Subject'] = "Activación cuenta de usuario"
+      # Cuerpo del mensaje se escoje texto plano
+      msg.attach(MIMEText(message_e, 'plain'))
+      #establecer conexión con el servidor en este caso gmail
+      server = smtplib.SMTP('smtp.gmail.com: 587')
+      server.starttls()
+      #autenticación de las credenciales de correo con el servidor de gmail
+      server.login(msg['From'], password)
+      #enviar el mensaje a travez del servidor de correo de gmail
+      server.sendmail(msg['From'], msg['To'], msg.as_string())
+      server.quit()#cerrar la conexión con el servidor
+
       if result == 'success':
          flash('Dirijase a su correo y active su cuenta para ingresar')
          return redirect(url_for('login'))
@@ -50,5 +73,3 @@ def singup():
       else:
          flash('El usuario no existe')
          return redirect(url_for('login'))
-
-
