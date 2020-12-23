e36edcc8d92d142094892c07c651ed10b4b257a8
Lcampillo <75735317+Lcampillo@users.noreply.github.com>
2020-12-22 20:15:28 -0500

    Todo

--- controllers/AuthController.py
+++ controllers/AuthController.py
@@ -3,7 +3,6 @@ from models import Auth
 from flask import render_template
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from random import choice
 import smtplib
 from models.Auth import db
 import urllib.parse as urlparse
@@ -13,7 +12,12 @@ msg = MIMEMultipart()
 
 @app.route('/login')
 def login():
-   return render_template('auth/login.html')
+      return render_template('auth/login.html')
+
+@app.route('/logout')
+def logout():
+      session.pop("user",None)
+      return render_template('auth/login.html')
 
 @app.route('/register')
 def register():
@@ -27,11 +31,19 @@ def verify():
       email = parse_qs(parsed.query)['email'][0]
       temp_key = parse_qs(parsed.query)['hash'][0]
 
-      for idx, account in enumerate(db):       
-           if email in account[0] and temp_key in account[2]:
-                  db[idx][3]= 'true'
-                  return render_template('auth/verify.html', email=email, temp_key=temp_key)
-      return redirect(url_for('register'))
+      print(parsed,email,temp_key,'\n')
+
+      cursor.execute("UPDATE users SET activated = 'true' WHERE email = ? AND hash = ?",(email,temp_key))
+      db.commit()
+
+      cursor.execute("SELECT * FROM users WHERE email = ? ", (email,))
+      result = cursor.fetchone()
+
+      if email == result[6]:
+         return render_template('auth/verify.html', email=email, temp_key=temp_key)
+      else:
+         flash('Algo salio mal')
+         return redirect(url_for('register'))
  
 
 @app.route('/recuperar_contraseña')
@@ -46,50 +58,47 @@ def store():
       email = request.form['email']
       cellphone = request.form['cellphone']
       password = request.form['password']
-      temp_key = generate_key()
 
       hazEl = Auth.Auth(name,surname,email,cellphone,password)
-      # Crea clave temporal para la activación de cuenta a través de correo
-      hazEl.set_temp_key(temp_key)
 
       result = hazEl.register()
 
-      ## Enviar email de activación de cuenta
-      message_e = f'''Bienvenido a Bloggi!!
+      if result[0] >= 1:
+         ## Enviar email de activación de cuenta
+         message_e = f"""Bienvenido a Bloggi!!
 
-      Hola {name.capitalize()} {surname.capitalize()}.
+         Hola {name.capitalize()} {surname.capitalize()}.
 
-      Nos place que empieces a utilizar nuestros servicios de Blog, estos son tus datos de acceso:
-      
-      ----------------------------
-      Correo: {email}
-      Contraseña: {password}
-      ----------------------------
+         Nos place que empieces a utilizar nuestros servicios de Blog, estos son tus datos de acceso:
 
-      Por favor, activa tu cuenta presionando el siguiente enlace:
-      http://127.0.0.1:5000/verify?email={hazEl.email}&hash={hazEl.activation_key}'''
+         ----------------------------
+         Correo: {email}
+         Contraseña: {password}
+         ----------------------------
 
-      #parametros de conexión del correo electronico
-      password = "yoyito2020"
-      msg['From'] = "profeyoyito2020@gmail.com"
-      msg['To'] = email
-      msg['Subject'] = "Activación cuenta de usuario"
-      # Cuerpo del mensaje se escoje texto plano
-      msg.attach(MIMEText(message_e, 'plain'))
-      #establecer conexión con el servidor en este caso gmail
-      server = smtplib.SMTP('smtp.gmail.com: 587')
-      server.starttls()
-      #autenticación de las credenciales de correo con el servidor de gmail
-      server.login(msg['From'], password)
-      #enviar el mensaje a travez del servidor de correo de gmail
-      server.sendmail(msg['From'], msg['To'], msg.as_string())
-      server.quit()#cerrar la conexión con el servidor
+         Por favor, activa tu cuenta presionando el siguiente enlace:
+         http://127.0.0.1:5000/verify?email={email}&hash={result[3]}"""
 
-      if result == 'success':
+         #parametros de conexión del correo electronico
+         password = "yoyito2020"
+         msg['From'] = "profeyoyito2020@gmail.com"
+         msg['To'] = email
+         msg['Subject'] = "Activación cuenta de usuario"
+         # Cuerpo del mensaje se escoje texto plano
+         msg.attach(MIMEText(message_e, 'plain'))
+         #establecer conexión con el servidor en este caso gmail
+         server = smtplib.SMTP('smtp.gmail.com: 587')
+         server.starttls()
+         #autenticación de las credenciales de correo con el servidor de gmail
+         server.login(msg['From'], password)
+         #enviar el mensaje a travez del servidor de correo de gmail
+         server.sendmail(msg['From'], msg['To'], msg.as_string())
+         server.quit()#cerrar la conexión con el servidor
+
          flash('Dirijase a su correo y active su cuenta para ingresar')
          return redirect(url_for('login'))
       else:
-         flash('El correo ya se encuentra registrado')
+         flash('Algo salio mal intentalo nuevamente')
          return redirect(url_for('register'))
 
 @app.route('/singup', methods=['GET','POST'])
@@ -100,22 +109,20 @@ def singup():
 
       hazEl = Auth.Auth('','',email,'',password)
 
-      account_user, activated_user = hazEl.login()
+      login = hazEl.login()
 
-      if account_user == 'true' and activated_user == 'true':
-         flash('Bienvenido')
+      print(login,'\n',email)
+
+      if login[6] == email and login[7] == 'true':
+         session["user"] = email
+         session["id"] = login[0]
+         flash(f"Bienvenido {login[2]}")
          return redirect(url_for('home'))
-      elif account_user == 'true' and activated_user == 'false':
+      elif login[7] == 'false':
          flash('El usuario no ha sido activado')
          return redirect(url_for('login'))
       else:
          flash('El usuario no existe')
          return redirect(url_for('login'))
 
-def generate_key():
-   longitud = 18
-   valores = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ=@#%&+"
 
-   p = ""
-   p = p.join([choice(valores) for i in range(longitud)])
-   return p
